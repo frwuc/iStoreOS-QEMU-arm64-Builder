a0e6195fbf7ae266e50e61eabe754dea903c9438
frwuc <frwuc@users.noreply.github.com>
2026-06-22 22:38:06 +0800

    fix: exclude dnsmasq to avoid conflict with dnsmasq-full

--- armsr-armv8/build-istoreos.sh
+++ armsr-armv8/build-istoreos.sh
@@ -30,7 +30,7 @@ cat files/etc/config/pppoe-settings
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建iStoreOS QEMU-arm64固件..."
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
-PACKAGES=""
+PACKAGES="-dnsmasq"
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
